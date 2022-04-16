49e9f15bda1943c8550736b1c1d99597b73abe2e
Andrei V <nihlete@gmail.com>
2022-04-16 23:39:45 +0300

    WIP: some evaluation, problems with priority

--- src/Lib.hs
+++ src/Lib.hs
@@ -38,49 +38,49 @@ instance Applicative Parser where
 
 instance Alternative Parser where
   empty = Parser (const [])
-  a <|> b = Parser (\s -> take 1 $ runParser a s ++ runParser b s) 
+  a <|> b = Parser (\s -> take 1 $ runParser a s ++ runParser b s)
 
-parseString :: String -> Parser a -> Maybe a
-parseString s (Parser p) = case p s of
+alt :: Parser a -> Parser a -> Parser a
+a `alt` b = Parser (\s -> runParser a s ++ runParser b s)
+
+parse :: String -> Parser a -> Maybe a
+parse s (Parser p) = case p s of
   [(val, "")] -> Just val
   _ -> Nothing
 
 skip :: (Char -> Bool) -> Parser ()
 skip p = Parser (\s -> [((), dropWhile p s)])
 
-parseChar :: (Char -> Bool) -> Parser Char
-parseChar pred = Parser g
+charParser :: (Char -> Bool) -> Parser Char
+charParser pred = Parser g
   where
     g "" = []
     g (c : ss)
       | pred c = [(c, ss)]
       | otherwise = []
 
-parseInt :: Parser Int
-parseInt = Parser g
+intParser :: Parser Int
+intParser = Parser g
   where
     g s = do
-      let res = runParser (some (parseChar isDigit)) s
+      let res = runParser (some (charParser isDigit)) s
       if null res then empty else pure (first read (head res))
 
-parseDouble :: Parser Double
-parseDouble = Parser g
+doubleParser :: Parser Double
+doubleParser = Parser g
   where
     g s = do
-      let res = runParser (some (parseChar validChar)) s
+      let res = runParser (some (charParser validChar)) s
       if null res then empty else pure (first read (head res))
     validChar c = isDigit c || (c == '.')
-
-exprParser :: Parser Expr
-exprParser = constParser <|> parenParser <|> negParser <|> binaryParser
 
 constParser :: Parser Expr
 constParser = ConstExpr <$> Parser g
   where
     g s = helper r1 r2
       where
-        r1 = runParser parseInt s
-        r2 = runParser parseDouble s
+        r1 = runParser intParser s
+        r2 = runParser doubleParser s
         helper [(x, ss)] [] = [(Integer x, ss)]
         helper [] [(x, ss)] = [(Floating x, ss)]
         helper [(x1, s1)] [(x2, s2)] = if length s2 < length s1 then [(Floating x2, s2)] else [(Integer x1, s1)]
@@ -96,20 +96,37 @@ opParser = Parser g
       [('/', ss)] -> [(Div, ss)]
       _ -> []
       where
-        result = runParser (parseChar (== '+') <|> parseChar (== '-') <|> parseChar (== '*') <|> parseChar (== '/')) s
+        result = runParser (charParser (== '+') <|> charParser (== '-') <|> charParser (== '*') <|> charParser (== '/')) s
 
 binaryParser :: Parser Expr
 binaryParser =
   BinaryExpr
-    <$> exprParser
+    <$> (negParser <|> parenParser <|> constParser)
     <*> (skip (== ' ') *> opParser <* skip (== ' '))
     <*> exprParser
 
 parenParser :: Parser Expr
-parenParser = ParenExpr <$> (parseChar (== '(') *> exprParser <* parseChar (== ')'))
+parenParser = ParenExpr <$> (charParser (== '(') *> exprParser <* charParser (== ')'))
 
 negParser :: Parser Expr
-negParser = NegExpr <$> (parseChar (== '-') *> exprParser)
+negParser = NegExpr <$> (charParser (== '-') *> exprParser)
+
+exprParser :: Parser Expr
+exprParser = negParser <|> parenParser <|> binaryParser <|> constParser
+
+
+-- todo: 
+-- ghci> evaluateExpr  . fst . head $ runParser binaryParser "2 * 5 + 1 "
+-- 12.0
+evaluateExpr :: Expr -> Double
+evaluateExpr (ConstExpr (Floating x)) = x
+evaluateExpr (ConstExpr (Integer x)) = fromIntegral x
+evaluateExpr (NegExpr e) = -(evaluateExpr e)
+evaluateExpr (ParenExpr e) = evaluateExpr e
+evaluateExpr (BinaryExpr a Add b) = evaluateExpr a + evaluateExpr b
+evaluateExpr (BinaryExpr a Sub b) = evaluateExpr a - evaluateExpr b
+evaluateExpr (BinaryExpr a Mult b) = evaluateExpr a * evaluateExpr b
+evaluateExpr (BinaryExpr a Div b) = evaluateExpr a / evaluateExpr b
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
